5f421a02641af05494838c33e5426aad30a5a917
ShioMisaka <94329511+ShioMisaka@users.noreply.github.com>
2026-04-29 21:12:45 +0800

    补充 which-key 缺失的代码分组和独立快捷键描述

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
   opts = {
     spec = {
       { "<leader>b", group = "Buffer" },
+      { "<leader>c", group = "代码" },
       { "<leader>d", group = "诊断" },
       { "<leader>f", group = "查找" },
       { "<leader>g", group = "Git" },
@@ -11,6 +12,8 @@ return {
       { "<leader>s", group = "会话" },
       { "<leader>v", group = "垂直拆分" },
       { "<leader>x", group = "窗口" },
+      { "<leader>w", desc = "格式化并保存", icon = "" },
+      { "<leader>-", desc = "水平拆分窗口", icon = "" },
     },
   },
 }
